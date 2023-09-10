62623ed1d40d82a040ecfc5e238beeefc7c1fe88
danielloquillo <danielbarria_1987@hotmail.com>
2023-09-10 15:58:02 -0400

    Get all products

--- src/contexts/ProductContext.js
+++ src/contexts/ProductContext.js
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, {createContext, useState, useEffect} from 'react';
 
-const ProductContext = () => {
-  return <div>ProductContext</div>;
+// create context
+
+export const ProductContext = createContext();
+
+// The parameter ({children}) of the function uses
+// destructuring objects. To indicate that the
+// ProductProvider function takes an object as an argument and
+// extracts the children property of that object. the property
+// children is to represent the content nested within
+// of the component.
+
+const ProductProvider = ({children}) => {
+
+// products state
+
+const [products, setProducts] =useState([]);
+
+// fetch product
+
+useEffect (() => {
+
+  const fecthProducts = async () => {
+
+    const response = await fetch('https://fakestoreapi.com/products');
+    const data = await response.json();
+    setProduct(data);
+
+  };
+
+  fecthProducts();
+
+// [] It is an empty list, making this list empty makes
+// let this be called only once
+
+ }, []);
+
+  return <ProductContext.Provider value={{products}}>
+    {children}
+    </ProductContext.Provider>;
+
 };
 
-export default ProductContext;
+export default ProductProvider;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, {useContext} from 'react';
+
+//import product.context
+import {ProductContext} from '../contexts/ProductContext';
 
 const Home = () => {
+  //get products from product context
+  
+
   return <div>Homepage</div>;
 };
 
